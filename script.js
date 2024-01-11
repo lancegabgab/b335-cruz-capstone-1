075c976ce2ac7cc3dea68747abb9d42af5a2db2c
lancegabgab <lancegabcruz@gmail.com>
2024-01-12 00:26:16 +0800

    added JQuery animations on index.html

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ $(document).ready(function () {
         $(this).removeClass("glow");
     });
 });
+
+//Index.html intro pics animation
+$(document).ready(function() {
+    var images = $('.fade-image');
+    var currentIndex = 0;
+
+    function fadeInOut() {
+      var currentImage = images.eq(currentIndex);
+
+      currentImage.fadeIn(200).delay(3000).fadeOut(200, function() {
+        currentIndex = (currentIndex + 1) % images.length;
+        fadeInOut();
+      });
+    }
+
+    fadeInOut(); // Start the initial fade in/out
+});
